b1239f6ec018403126bb51195502d9baa330a6fe
Marcos <marcosgabriel79355@gmail.com>
2022-05-20 17:30:24 -0300

    update

--- Bluefood/src/main/java/br/com/softblue/bluefood/application/service/ValidationExpeception.java
+++
@@ -1,9 +0,0 @@
-package br.com.softblue.bluefood.application.service;
-
-@SuppressWarnings("serial")
-public class ValidationExpeception  extends Exception{
-  
-	public ValidationExpeception(String message) {
-		super(message);
-	}
-}
--- Bluefood/src/main/java/br/com/softblue/bluefood/infrastructure/security/Role.java
+++
@@ -1,5 +0,0 @@
-package br.com.softblue.bluefood.infrastructure.security;
-
-public enum Role {
-	CLIENTE, RESTAURANTE;
-}
