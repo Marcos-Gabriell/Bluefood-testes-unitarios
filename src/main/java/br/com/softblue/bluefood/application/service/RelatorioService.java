3ced96a3b643f2c214e0305a792ed4ba98ac96d5
Marcos Gabriel <marcosgabriel79355@gmail.com>
2022-08-15 14:58:17 -0300

    Testes Unitarios

--- src/main/java/br/com/softblue/bluefood/application/service/RelatorioService.java
+++ src/main/java/br/com/softblue/bluefood/application/service/RelatorioService.java
@@ -18,67 +18,68 @@ import br.com.softblue.bluefood.util.CollectionUtils;
 
 @Service
 public class RelatorioService {
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
 	public List<Pedido> listPedidos(Integer restauranteId, RelatorioPedidoFilter filter) {
 
 		Integer pedidoId = filter.getPedidoId();
-		
+
 		if (pedidoId != null) {
 			Pedido pedido = pedidoRepository.findByIdAndRestaurante_Id(pedidoId, restauranteId);
 			return CollectionUtils.listOf(pedido);
 		}
-		
+
 		LocalDate dataInicial = filter.getDataInicial();
 		LocalDate dataFinal = filter.getDataFinal();
-		
+
 		if (dataInicial == null) {
 			return CollectionUtils.listOf();
 		}
-		
+
 		if (dataFinal == null) {
 			dataFinal = LocalDate.now();
 		}
-		
-		return pedidoRepository.findByDateInterval(restauranteId, dataInicial.atStartOfDay(), dataFinal.atTime(23, 59, 59));
+
+		return pedidoRepository.findByDateInterval(restauranteId, dataInicial.atStartOfDay(),
+				dataFinal.atTime(23, 59, 59));
 	}
-	
+
 	public List<RelatorioItemFaturamento> calcularFaturamentoItens(Integer restauranteId, RelatorioItemFilter filter) {
 		List<Object[]> itensObj;
-		
+
 		Integer itemId = filter.getItemId();
 		LocalDate dataInicial = filter.getDataInicial();
 		LocalDate dataFinal = filter.getDataFinal();
-		
+
 		if (dataInicial == null) {
 			return CollectionUtils.listOf();
 		}
-		
+
 		if (dataFinal == null) {
 			dataFinal = LocalDate.now();
 		}
-		
+
 		LocalDateTime dataHoraInicial = dataInicial.atStartOfDay();
 		LocalDateTime dataHoraFinal = dataFinal.atTime(23, 59, 59);
-		
+
 		if (itemId != 0) {
 			itensObj = pedidoRepository.findItensForFaturamento(restauranteId, itemId, dataHoraInicial, dataHoraFinal);
-		
+
 		} else {
 			itensObj = pedidoRepository.findItensForFaturamento(restauranteId, dataHoraInicial, dataHoraFinal);
 		}
-		
+
 		List<RelatorioItemFaturamento> itens = new ArrayList<>();
-		
+
 		for (Object[] item : itensObj) {
 			String nome = (String) item[0];
 			Long quantidade = (Long) item[1];
 			BigDecimal valor = (BigDecimal) item[2];
 			itens.add(new RelatorioItemFaturamento(nome, quantidade, valor));
 		}
-		
+
 		return itens;
 	}
 }
--- src/test/java/br/com/softblue/bluefood/repository/CategoriaRestauranteRepositoryTest2.java
+++ src/test/java/br/com/softblue/bluefood/repository/CategoriaRestauranteRepositoryTest2.java
@@ -2,6 +2,8 @@ package br.com.softblue.bluefood.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,9 +12,10 @@ import org.springframework.test.context.ActiveProfiles;
 
 import br.com.softblue.bluefood.domain.restaurante.CategoriaRestaurante;
 
+
 @DataJpaTest
 @ActiveProfiles("teste")
-class CategoriaRestauranteRepositoryTest {
+class CategoriaRestauranteRepositoryTest2 {
 
 	@Autowired
 	private TestEntityManager em;
@@ -32,10 +35,10 @@ class CategoriaRestauranteRepositoryTest {
 		 CategoriaRestaurante cr2 = em.find(CategoriaRestaurante.class, cr.getId());
 		 assertThat(cr.getNome()).isEqualTo(cr2.getNome());
 		 
+
+
+		 em.remove(cr);
 		 
-		 
-		em.remove(cr);
-		 
-		 assertThat(em.find(CategoriaRestaurante.class, cr.getId())).isNull();;
+		 assertThat(em.find(CategoriaRestaurante.class, cr.getId()));
 	}
 }
